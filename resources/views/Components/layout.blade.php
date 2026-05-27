6e0eb9229fd1138d2831a6cde0f0a6562d70495a
Joaquin Viera <joaquin.viera@lightit.io>
2026-05-27 15:46:48 -0300

    feat: nav link component

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -18,15 +18,13 @@
                         <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
                             alt="Your Company" class="size-8" />
                     </div>
+                    {{-- Web View --}}
                     <div class="hidden md:block">
                         <div class="ml-10 flex items-baseline space-x-4">
                             <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                            <a href="/" aria-current="page"
-                                class="rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white">Home</a>
-                            <a href="/about"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
-                            <a href="/contact"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
+                            <x-nav-link :active="request()->path() === '/'" href="/">Home</x-nav-link>
+                            <x-nav-link :active="request()->path() === 'about'" href="/about">About</x-nav-link>
+                            <x-nav-link :active="request()->path() === 'contact'" href="/contact">Contact</x-nav-link>
 
                         </div>
                     </div>
@@ -84,11 +82,11 @@
             <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
                 <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
                 <a href="/" aria-current="page"
-                    class="block rounded-md bg-gray-950/50 px-3 py-2 text-base font-medium text-white">Home</a>
+                    class="block rounded-md px-3 py-2 text-base font-medium text-white {{ request()->path() === '/' ? 'bg-gray-950/50' : 'text-gray-300 hover:bg-white/5' }}">Home</a>
                 <a href="/about"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
+                    class="block rounded-md px-3 py-2 text-base font-medium text-white {{ request()->path() === 'about' ? 'bg-gray-950/50' : 'text-gray-300 hover:bg-white/5' }}">About</a>
                 <a href="/contact"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
+                    class="block rounded-md px-3 py-2 text-base font-medium text-white {{ request()->path() === 'contact' ? 'bg-gray-950/50' : 'text-gray-300 hover:bg-white/5' }}">Contact</a>
 
             </div>
             <div class="border-t border-white/10 pt-4 pb-3">
--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,1 +1,6 @@
- <a {{ $attributes }}>{{ $slot }}</a>
+@props(['active' => false])
+
+<a {{ $attributes->merge(['href' => '/']) }} aria-current="{{ $active ? 'page' : 'false' }}"
+    class="{{ $active ? 'bg-blue-500 text-white p-1 rounded-lg' : 'text-gray-300 hover:bg-gray-200 hover:text-white' }}">
+    {{ $slot }}
+</a>
